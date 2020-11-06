964209f715ab86701172f67c37d16cdd815e2023
Ujjwal-Shekhawat <ujjus2001@gmail.com>
2020-11-06 18:21:03 +0530

    code cleanup and proper mail templates

--- utils/sendMail.js
+++ utils/sendMail.js
@@ -1,6 +1,7 @@
 const nodemailer = require('nodemailer');
+const hbs = require('nodemailer-express-handlebars');
 
-const sendMail = async (opts) => {
+const sendMail = async (opts, data) => {
   const transport = nodemailer.createTransport({
     // host: process.env.SMTP_HOST,
     host: `smtp.mailtrap.io`,
@@ -11,17 +12,37 @@ const sendMail = async (opts) => {
     },
   });
 
+  transport.use(
+    'compile',
+    hbs({
+      viewEngine: 'express-handlebars',
+      viewPath: './views/',
+    })
+  );
+
   const info = {
     from: `Test <noreply@test@nrep.com>`,
     to: opts.email,
     subject: opts.subject,
     text: opts.message,
-    html: opts.html,
+    // html: opts.html,
+    template: 'index',
+    context: {
+      username: data.username,
+      email: data.email,
+      createAt: data.date,
+    },
   };
 
-  const result = await transport.sendMail(info);
+  const result = await transport.sendMail(info, function (err, info) {
+    if (err) {
+      console.log(`Error`, err);
+    } else {
+      console.log(`Message sent`);
+    }
+  });
 
-  console.log(result.messageId);
+  // console.log(result.messageId);
 };
 
 module.exports = sendMail;
