6094fca88c233b108722db559a07d094a3597f32
Abhishek Raj <abhishekraj272@gmail.com>
2020-11-13 21:26:38 +0530

    removed cors

--- server.js
+++ server.js
@@ -21,6 +21,12 @@ const apiLimiter = rateLimit({
   message: { message: 'We dont allow too many requests from same IP.' },
 });
 
+app.all('/', function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
 app.use('/api/', apiLimiter);
 
 app.get('/', (req, res) => {
